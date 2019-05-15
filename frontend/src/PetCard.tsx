09cdf1042da14533bfc2fa1dcf38cd2bc4fb021e
charlenezheng <1847602422@qq.com>
2019-05-15 18:59:17 +0100

    step 2 Build a user interface for the animal shelter app

--- frontend/src/PetCard.tsx
+++ frontend/src/PetCard.tsx
@@ -1,8 +1,29 @@
 import React, { Component } from 'react';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
 
-class PetCard extends Component {
+interface Pet {
+  readonly id: string;
+  name: string;
+  description: string;
+}
+
+type Props = {
+  pet: Pet;
+};
+
+class PetCard extends Component<Props> {
   render() {
-      return <div>I'm a pet card!</div>;
+      return (
+      <Card style={{ width: '18rem' }}>
+        <Card.Img variant="top" src={"https://source.unsplash.com/collection/212527/200x200/?sig=" + Math.floor(Math.random() * 100)} />
+        <Card.Body>
+          <Card.Title>{this.props.pet.name}</Card.Title>
+          <Card.Text>{this.props.pet.description}</Card.Text>
+          <Button variant="primary">Adopt</Button>
+        </Card.Body>
+      </Card>
+);
   }
 }
 
